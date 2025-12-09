014551f3ba12552bedb3795d4c77ef1720f3288b
VladN <netuvladika@gmail.com>
2025-12-09 20:07:38 +0300

    add somephing

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your models here.
+class Role(models.Model):
+    name = models.DateTimeField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class CustomUserManager(UserManager):
+
+    def _resolve_role(self, value):
+        if isinstance(value, Role):
+            return value
+        try:
+            # === SELECT * FROM Role WHERE id = 1;
+            return Role.objects.get(id=value)
+        except (ValueError, ObjectDoesNotExist):
+            # == SELECT * FROM Role WHERE name = 'owner';
+            return Role.objects.get(name=value)
+
+
+    def create_user(self, username, email, password, **extra_fields):
+        extra_fields['role'] = self._resolve_role(extra_fields['role'])
+        return super().create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields['role'] = self._resolve_role(value='owner')
+        extra_fields['is_superuser'] = True
+        extra_fields['is_staff'] = True
+
+        return super().create_superuser(username=username, password=password, **extra_fields)
+
+
+class User(AbstractUser):
+    role = models.ForeignKey(Role, null=False, blank=False, on_delete=models.PROTECT)
+
+    objects = CustomUserManager()
+
+    REQUIRED_FIELDS = ['role', 'email']
+
+
+
+
+
+
